af64ce66498e9eaa89811e712e98c578f0c37142
bon <bona.wasan@gmail.com>
2021-11-10 18:10:02 +0700

    slice update

--- godemo/slice.go
+++ godemo/slice.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var s []byte
+	var s []byte // array
 	s = make([]byte, 6, 9) //([]type, len,cap)
 	s = []byte{2, 4, 6, 8, 1, 3, 5, 7}
 
 	fmt.Println(len(s), cap(s), s[2:5])
+
 
 	d := []byte{'r', 'o', 'a', 'd'}
 	e := d[2:]
@@ -15,4 +16,6 @@ func main() {
 	e[1] = 'm'
 	// e == []byte{'a', 'm'}
 	// d == []byte{'r', 'o', 'a', 'm'}
+
+	
 }
