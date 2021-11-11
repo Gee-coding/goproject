7fb9c23a26f7bcdd7ddbf1b8a952bff40bebdd46
bon <bona.wasan@gmail.com>
2021-11-11 14:17:22 +0700

    import test

--- echotest/controllers/usercontroller.go
+++ echotest/controllers/usercontroller.go
@@ -1,13 +1,21 @@
 package controllers
 
+import (
+	"fmt"
+	"github.com/goproject/models"
+)
+
 func GetUser() {
 
 }
 
-func addUser(){
+func addUser(i int, u ...usermodel.Usermodel) {
 
+	var newuser = make(map[int]interface{})
+	newuser[i] = u
+	fmt.Println(newuser)
 }
 
 func deleteUser() {
-	
+
 }
--- echotest/models/usermodel.go
+++ echotest/models/usermodel.go
@@ -1,10 +1,8 @@
 package models
 
-type User struct {
-	
-	Id uint `json:"id"`
-	Name string `json:"name"`
+type UserModel struct {
+	Id       uint   `json:"id"`
+	Name     string `json:"name"`
 	Position string `json:"position"`
-	Age int `json:"age"`
-
+	Age      int    `json:"age"`
 }
